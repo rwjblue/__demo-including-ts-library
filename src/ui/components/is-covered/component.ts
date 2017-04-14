ff552411f417cee3dc74487d4dbc886ab6a05e77
Robert Jackson <rjackson@linkedin.com>
2017-04-14 12:13:03 -0400

    Use ts-library-starter in a component.

--- src/ui/components/is-covered/component.ts
+++ src/ui/components/is-covered/component.ts
@@ -1,5 +1,9 @@
 import Component from "@glimmer/component";
+import LibraryStarter from 'ts-library-starter';
 
 export default class DemoIncludingTsLibrary extends Component {
-
+  get isCovered() {
+    let example = new LibraryStarter.Example();
+    return example.covered;
+  }
 }
